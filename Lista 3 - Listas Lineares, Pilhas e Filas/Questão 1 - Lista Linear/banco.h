9f364307d368524a19208649443d07513706b8bc
IgorCRH <125159948+IgorCRH@users.noreply.github.com>
2023-05-20 13:06:44 -0300

    Update banco.h

--- Lista 3 - Listas Lineares, Pilhas e Filas/Questão 1 - Lista Linear/banco.h
+++ Lista 3 - Listas Lineares, Pilhas e Filas/Questão 1 - Lista Linear/banco.h
@@ -16,54 +16,54 @@ typedef struct {
     int ultimo;
 } Lista;
 
-// FunÁıes
+// Fun√ß√µes
 void inicializarLista(Lista *lista, int capacidade) { // Inicializa a Lista
-    lista->capacidade = capacidade; // Passa e determina a capacidade por par‚metro a partir da main
-    lista->dados = (TCliente*) malloc(capacidade * sizeof(TCliente)); // AlocaÁ„o din‚mica dos dados da struct, que no caso s„o os dos clientes
-    lista->ultimo = -1; // Define que a lista est· vazia antes de ter alguÈm cadastrado
+    lista->capacidade = capacidade; // Passa e determina a capacidade por par√¢metro a partir da main
+    lista->dados = (TCliente*) malloc(capacidade * sizeof(TCliente)); // Aloca√ß√£o din√¢mica dos dados da struct, que no caso s√£o os dos clientes
+    lista->ultimo = -1; // Define que a lista est√° vazia antes de ter algu√©m cadastrado
 }
 
 int buscarLista(Lista *lista, int cpf) { // Busca CPF do cliente pela lista
-    for (int i = 0; i <= lista->ultimo; i++) { // For que vai do 0 atÈ a posiÁ„o do ˙ltimo cliente da lista
-        if (lista->dados[i].cpf == cpf) { // Se encontrar o CPF, comparando o CPF da struct Lista com o que È passado pela main, retorna o Ìndice do elemento na lista
+    for (int i = 0; i <= lista->ultimo; i++) { // For que vai do 0 at√© a posi√ß√£o do √∫ltimo cliente da lista
+        if (lista->dados[i].cpf == cpf) { // Se encontrar o CPF, comparando o CPF da struct Lista com o que √© passado pela main, retorna o √≠ndice do elemento na lista
             return i;
         }
     }
-    return -1; // Caso contr·rio, retorna -1 indicando que o elemento n„o foi encontrado
+    return -1; // Caso contr√°rio, retorna -1 indicando que o elemento n√£o foi encontrado
 }
 
 int inserirCliente(Lista *lista, TCliente cliente) { // Cadastra cliente no sistema
-    if (lista->ultimo == lista->capacidade - 1) { // Se a lista estiver cheia, n„o È mais possÌvel cadastrar, caso tenhamos capacidade fixa. Capacidade-1 pois os Ìndices dos elementos da lista comeÁam em 0.
+    if (lista->ultimo == lista->capacidade - 1) { // Se a lista estiver cheia, n√£o √© mais poss√≠vel cadastrar, caso tenhamos capacidade fixa. Capacidade-1 pois os √≠ndices dos elementos da lista come√ßam em 0.
         return 0;
     }
-    if (buscarLista(lista, cliente.cpf) != -1) { // Caso o cliente j· tenha sido cadastrado
+    if (buscarLista(lista, cliente.cpf) != -1) { // Caso o cliente j√° tenha sido cadastrado
         return -1;
     }
     lista->ultimo++;
     lista->dados[lista->ultimo] = cliente;
-    return 1; // Cliente È cadastrado
+    return 1; // Cliente √© cadastrado
 }
 
 int removerCliente(Lista *lista, int cpf) { // Remove cliente do sistema
-    int posicao = buscarLista(lista, cpf); // Posicao chama a funÁ„o buscar CPF na lista (sistema)
-    if (posicao == -1) { // Se buscar lista encontrar o CPF, retornar· -1, ent„o:
-        return 0; // cliente n„o encontrado
+    int posicao = buscarLista(lista, cpf); // Posicao chama a fun√ß√£o buscar CPF na lista (sistema)
+    if (posicao == -1) { // Se buscar lista n√£o encontrar o CPF, retornar√° -1, ent√£o:
+        return 0; // cliente n√£o encontrado
     }
-    for (int i = posicao; i < lista->ultimo; i++) { // Caso ele seja encontrado, o for È executado da posiÁ„o do cliente a ser removido atÈ o ˙ltimo
-        lista->dados[i] = lista->dados[i + 1]; // Copia cada cliente posterior a ele, para a posiÁ„o anterior
+    for (int i = posicao; i < lista->ultimo; i++) { // Caso ele seja encontrado, o for √© executado da posi√ß√£o do cliente a ser removido at√© o √∫ltimo
+        lista->dados[i] = lista->dados[i + 1]; // Copia cada cliente posterior a ele, para a posi√ß√£o anterior
     }
-    lista->ultimo--; // Decrementa o Ìndice do ˙ltimo cliente inserido na lista, indicando que a lista agora contÈm um cliente a menos
+    lista->ultimo--; // Decrementa o √≠ndice do √∫ltimo cliente inserido na lista, indicando que a lista agora cont√©m um cliente a menos
     return 1; // Cliente foi removido
 }
 
-int sacarLista(Lista *lista, int cpf, float valor) { // FunÁ„o de saque
-    int posicao = buscarLista(lista, cpf); // Posicao chama a funÁ„o buscar CPF na lista (sistema)
-    if (posicao == -1) { // Se buscar lista encontrar o CPF, retornar· -1, ent„o:
-        return 0; // cliente n„o encontrado
+int sacarLista(Lista *lista, int cpf, float valor) { // Fun√ß√£o de saque
+    int posicao = buscarLista(lista, cpf); // Posicao chama a fun√ß√£o buscar CPF na lista (sistema)
+    if (posicao == -1) { // Se buscar lista encontrar o CPF, retornar√° -1, ent√£o:
+        return 0; // cliente n√£o encontrado
     }
     if (lista->dados[posicao].saldo < valor) { // Se for encontrado, e o saldo for menor que a quantia:
         return -1; // saldo insuficiente
     }
-    lista->dados[posicao].saldo -= valor; // Se o saldo for maior, ent„o realiza-se o saque
+    lista->dados[posicao].saldo -= valor; // Se o saldo for maior, ent√£o realiza-se o saque
     return 1; // saque realizado com sucesso
 }
